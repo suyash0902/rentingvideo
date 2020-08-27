83ab51bf14c893fbd4b8d5bb9f48eeec22d3ce93
Suyash <suyash.sri,0902@gmail.com>
2020-08-27 13:47:56 +0530

    create videostore.java class

---
+++ src/defining/videostore.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Suyash Srivastava (suyash0902)
+ *   Date: 27-08-2020
+ *   Time: 13:47
+ *   File: videostore.java
+ */
+
+package defining;
+
+public class videostore {
+}
+

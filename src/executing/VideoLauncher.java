178d8b7471cb22bb6ba470c83d87de7627da0ffa
Suyash <suyash.sri,0902@gmail.com>
2020-08-27 15:04:01 +0530

    add const to denote menu options

--- src/executing/VideoLauncher.java
+++ src/executing/VideoLauncher.java
@@ -10,6 +10,13 @@ package executing;
 import java.util.Scanner;
 
 public class VideoLauncher {
+    public static final int ADD_Video = 1;
+    public static final int CHECK_OUT_Video = 2;
+    public static final int RETURN_Video = 3;
+    public static final int RECEIVE_Rating = 4;
+    public static final int LIST_Inventory = 5;
+    public static final int EXIT = 6;
+
     public static void main(String[] args) {
         System.out.println("MAIN MENU");
         System.out.println("=========");
@@ -21,6 +28,11 @@ public class VideoLauncher {
         System.out.println("Enter your choice from (1-6) : ");
         Scanner scanner = new Scanner(System.in);
         int customerInput = scanner.nextInt();
+        switch (customerInput) {
+            case ADD_Video:
+                break;
+
+        }
         scanner.close();
     }
 }

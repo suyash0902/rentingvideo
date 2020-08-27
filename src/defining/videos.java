8eb24be75149d5f6411f6d3329bfe66550db2ebc
Suyash <suyash.sri,0902@gmail.com>
2020-08-27 11:58:33 +0530

    introduce getter and setter method for the member variables and executing them in videos.java and main.java

---
+++ src/defining/videos.java
@@ -0,0 +1,41 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Suyash Srivastava (suyash0902)
+ *   Date: 27-08-2020
+ *   Time: 11:51
+ *   File: videos.java
+ */
+
+package defining;
+
+public class videos {
+    // Creating member variable
+    private String videoName;
+    private boolean checkOut;
+    private int rating;
+
+    //Setter Method & Getter Method
+    public String getVideoName() {
+        return videoName;
+    }
+
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public boolean isCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(boolean checkOut) {
+        this.checkOut = checkOut;
+    }
+}
+
---
+++ src/executing/main.java
@@ -0,0 +1,22 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Suyash Srivastava (suyash0902)
+ *   Date: 27-08-2020
+ *   Time: 11:53
+ *   File: main.java
+ */
+
+package executing;
+
+import defining.videos;
+
+public class main {
+    public static void main(String[] args) {
+        videos video = new videos();
+        video.setVideoName("1");
+        video.setRating(6);
+        video.setCheckOut(true);
+        System.out.println("video name : " + video.getVideoName() + "," + "rating : " + video.getRating() + "," + "is video available : " + video.isCheckOut() + ".");
+
+    }
+}
+
